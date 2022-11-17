7bb18c440f9b8a7e4032f842f0431d24abd4ef46
UrosQacode <uros.zivadinovic7@gmail.com>
2022-11-17 13:41:38 +0100

    Init

---
+++ SeleniumPOM/Page/HomePage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using WebDriver = SeleniumPOM.Driver.WebDriver;
+
+namespace SeleniumPOM.Page;
+
+public class HomePage
+{
+    private IWebDriver _driver = WebDriver.Instance;
+    public IWebElement Title => _driver.FindElement(By.Id("TitleId"));
+    public IWebElement Initial => _driver.FindElement(By.Id("Initial"));
+    public IWebElement FirstName => _driver.FindElement(By.Id("FirstName"));
+    public IWebElement MiddleName => _driver.FindElement(By.Id("MiddleName"));
+    public IWebElement Gender => _driver.FindElement(By.Name("Male"));
+    public IWebElement LanguagesKnown => _driver.FindElement(By.Name("english"));
+
+
+
+
+    public void SelectOption(string option)
+    {
+        SelectElement drop = new SelectElement(_driver.FindElement(By.Id("TitleId")));
+        drop.SelectByText(option); // ovo vam sluzi da izabere iz drop downa Mr. ili Ms. potrebno je da mu u test kazete sta od ova dva zelite da izbare
+    }
+
+
+}
---
+++ SeleniumPOM/Page/LoginPage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using WebDriver = SeleniumPOM.Driver.WebDriver;
+
+namespace SeleniumPOM.Page
+{
+    public class LoginPage
+    {
+
+        private IWebDriver _driver = WebDriver.Instance;
+        public IWebElement UserName => _driver.FindElement(By.XPath("//*[@id=\"userName\"]/p[1]/input"));
+        public IWebElement Password => _driver.FindElement(By.XPath("//*[@id=\"userName\"]/p[2]/input"));
+        public IWebElement LoginButton => _driver.FindElement(By.XPath("//*[@id=\"userName\"]/p[3]/input"));
+
+        
+            
+        public void LoginOnPage(string name, string password)
+        {
+           
+           UserName.SendKeys(name);
+            Password.SendKeys(password);
+            LoginButton.Submit();
+        }
+    }
+}
